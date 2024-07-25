267af0aeed3c594db2ee304e859ae69769013085
Alfredo Alberto Palacios Rodriguez <yiyoaapr@gmail.com>
2024-07-24 23:34:41 -0600

    Inicializando el repositorio con todos los archivos del proyecto

---
+++ src/main/java/com/yiyostore/yiyostore_administracion_pedidos/YiyostoreAdministracionPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.yiyostore.yiyostore_administracion_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiyostoreAdministracionPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiyostoreAdministracionPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yiyostore/yiyostore_administracion_pedidos/YiyostoreAdministracionPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiyostore.yiyostore_administracion_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiyostoreAdministracionPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
