f7a376a446c2deb80c6ede996bde6bb3350afc95
Alfredo Alberto Palacios Rodriguez <yiyoaapr@gmail.com>
2024-07-26 18:07:21 -0600

    Creacion de los modelos Direccion, Ciudad y Colonia que serán usados como atributo en el modelo Cliente

---
+++ src/main/java/com/yiyostore/yiyostore_administracion_pedidos/models/Ciudad.java
@@ -0,0 +1,10 @@
+package com.yiyostore.yiyostore_administracion_pedidos.models;
+
+public enum Ciudad {
+    TORREÓN,
+    GÓMEZ_PALACIO,
+    LERDO,
+    MATAMOROS,
+    SAN_PEDRO,
+    FRANCISCO_I_MADERO
+}
